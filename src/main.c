fc2930d87454c74f8841e111e10ef1171eb7ad49
sanchesor <sanchesor@gmail.com>
2017-04-17 18:24:05 +0200

    save

--- src/main.c
+++ src/main.c
@@ -1,12 +1,30 @@
 #include "stm32f10x.h"
 #include "libssd1306.h"
 #include "movingrect.h"
-#include <math.h>
 
+#define T1 4027
+#define T2 3002
+#define T3 2246
+#define T4 1685
+#define T5 1337
+#define T6 1000
 
-volatile uint8_t tl[64];
-volatile int tl_index = 0;
+#define COUNTER_MAX 10000 - 1
 
+uint16_t TT[] = {T1, T2, T3, T4, T5, T6};
+
+volatile uint8_t lines[6][64];
+
+volatile uint16_t offset[] = {0,0,0,0,0,0};
+
+volatile uint16_t base_offset[] = {
+	COUNTER_MAX%T1, 
+	COUNTER_MAX%T2, 
+	COUNTER_MAX%T3, 
+	COUNTER_MAX%T4, 
+	COUNTER_MAX%T5, 
+	COUNTER_MAX%T6
+};
 
 void TIM2_IRQHandler()
 {
@@ -14,6 +32,10 @@ void TIM2_IRQHandler()
 	{
 		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 		
+		for(int i=0;i<6;i++)
+		{
+			offset[i] = (offset[i] + base_offset[i])%TT[i];
+		}
 
 	}
 }
@@ -49,8 +71,8 @@ void hardware_init()
 		
 	TIM_TimeBaseStructInit(&tim);
 	tim.TIM_CounterMode = TIM_CounterMode_Up;
-	tim.TIM_Prescaler = 7200-1;	// 72-1 -> period in us
-	tim.TIM_Period = 60000-1;
+	tim.TIM_Prescaler = 217;	// 72Mhz/217 -> 3us
+	tim.TIM_Period = COUNTER_MAX;
 	TIM_TimeBaseInit(TIM2, &tim);
 	
 	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
@@ -61,10 +83,26 @@ void hardware_init()
 	nvic.NVIC_IRQChannelSubPriority = 0;
 	nvic.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&nvic);
+
+
+	TIM_TimeBaseStructInit(&tim);
+	tim.TIM_CounterMode = TIM_CounterMode_Up;
+	tim.TIM_Prescaler = 7200-1;
+	tim.TIM_Period = 200;
+	TIM_TimeBaseInit(TIM3, &tim);
+	
+	TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+	TIM_Cmd(TIM3, ENABLE);
+	
+	nvic.NVIC_IRQChannel = TIM3_IRQn;
+	nvic.NVIC_IRQChannelPreemptionPriority = 0;
+	nvic.NVIC_IRQChannelSubPriority = 0;
+	nvic.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&nvic);
 	
 }
 
-
+/*
 void draw_timeline()
 {
 	int offset = 5;
@@ -72,55 +110,70 @@ void draw_timeline()
 	for(int i=0;i<64;i++)
 	{
 		drawLineH(offset, offset+tl[i], i);
-	}
+	}	
+}
+*/
+
+void draw_lines()
+{
+	int offset = 5;
+	int maxh = 20;
 	
+	for(int l=0;l<6;l++)
+	{
+		for(int i=0;i<64;i++)
+		{
+			drawLineH(offset + l*maxh, offset + l*maxh + lines[l][i], i);
+		}	
+	}
 }
 
-double speed=1.0;
-void calc_timeline()
+void calc_lines()
 {
+	//uint16_t v = ADC_GetConversionValue(ADC1);
+	uint16_t cur_v = 4;
+	
+	// normalize cur_v !
 	
 	uint16_t t = TIM_GetCounter(TIM2);
 	
-	/*
-	tl_index = t/10;
-	
-	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_6) == 0)
-		tl[tl_index] = tl[tl_index]+10;
-	else
-		tl[tl_index] = 0;	
-	*/
-	
-	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_6) == 0)
-		speed += 0.01*speed;
-	if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_7) == 0)
-		speed -= 0.01*speed;
-		
-	for(int i=0;i<63;i++)
+	for(int l=0;l<6;l++)
 	{
-		tl[i] = 30*sin(0.1*i + t*0.001*speed);
-	}
-	
+		uint16_t tp = (t + offset[l])%TT[l];		
+		int li = 64*tp/TT[l];
+		
+		lines[l][li] = cur_v;
+	}	
 }
+
+void TIM3_IRQHandler()
+{
+	if (TIM_GetITStatus(TIM3, TIM_IT_Update) == SET)
+	{
+		TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
+
+		ClearBuffer();		
+		draw_lines();
+		TransferBuffer();
+
+		
+
+	}
+}
+
 
 int main(void)
 {
 	hardware_init();
 	ssd1306init();
 	
+		
+	
 	while(1)
-	{					
-		
-		
-		ClearBuffer();
-		
-		calc_timeline();
-		draw_timeline();
+	{								
 
-		TransferBuffer();
-		
 
 		
-		
+
 	}
 }
